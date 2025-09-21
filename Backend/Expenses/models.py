757754bec8351e1bfa90a36beab19fcfc975a9c2
Aditya Nerkar <84532627+adityyyaaa777@users.noreply.github.com>
2025-09-21 20:04:28 +0530

    Created First Model and Registered App

--- Backend/Expenses/models.py
+++ Backend/Expenses/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Expense(models.Model):
+    title = models.Charfield(max_length = 80)
+    amount = models.DecimalField(max_digits = 10,decimal_place = 2)
+    category = models.CharField(max_length = 50, blank=True, null = True)
+    date = models.DateField(auto_now_add = True)
+
+    def __str__(self):
+        return f"{self.title} - {self.amount}"
