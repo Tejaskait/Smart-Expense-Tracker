1bae243c022b42dfe6353c46700780b2c299c80d
Aditya Nerkar <84532627+adityyyaaa777@users.noreply.github.com>
2025-09-21 20:17:23 +0530

    Fixed Syntax and Migrations

--- Backend/Expenses/admin.py
+++ Backend/Expenses/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Expense
 
-# Register your models here.
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('title','amount','category','date')   # show columns
+    search_fields = ('title','category')                  # search bar
+    list_filter = ('category','date')                     # filter
+
--- Backend/Expenses/models.py
+++ Backend/Expenses/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Expense(models.Model):
-    title = models.Charfield(max_length = 80)
-    amount = models.DecimalField(max_digits = 10,decimal_place = 2)
+    title = models.CharField(max_length = 80)
+    amount = models.DecimalField(max_digits = 10, decimal_places = 2)
     category = models.CharField(max_length = 50, blank=True, null = True)
     date = models.DateField(auto_now_add = True)
 
