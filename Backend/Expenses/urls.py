354a9ae91cb15aee19e7d1b86eed23386183887d
Aditya Nerkar <84532627+adityyyaaa777@users.noreply.github.com>
2025-10-21 10:04:28 +0530

    Added Upload Function and Integrated Gemini API

--- Backend/Expenses/urls.py
+++ Backend/Expenses/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('upload/', views.upload_expense_image, name='upload_expense'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('about/', views.about, name='about'),
     path('add-expense/', views.add_expense, name='add_expense'),
